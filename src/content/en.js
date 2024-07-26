975d847cd788184db7e7052b1537152a95f49a44
samdemaeyer <sam_demaeyer@hotmail.com>
2024-07-26 11:00:32 +0100

    [CHORE] - Update meta description

---
+++ src/content/en.js
@@ -0,0 +1,11 @@
+export const en = {
+  youAreInvited: 'You are invited to',
+  leoDeMaeyers: "Leo De Maeyer's",
+  babyBorrel: 'Baby Borrel',
+  august: 'August',
+  sunday: 'Sunday',
+  time: 'At 14:00',
+  addressLine1: 'Ontmoetingscentrum Heilig Kruis',
+  addressLine2: 'Kruisherenlaan 29',
+  addressLine3: '3500 Hasselt, België'
+}
